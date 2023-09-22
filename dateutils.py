544d36fdc6be702d22d0226743696fe2af47d1e3
kazukiotsuka <otsuka.kazuki@googlemail.com>
2023-09-22 13:14:45 +0900

    dateutils: update dateutils

--- dateutils.py
+++ dateutils.py
@@ -3,66 +3,88 @@
 #
 # helpers/dateutils.py
 #
-# usage:
-# from heplers.dateutils import datetime_to_iso8061, iso8061_to_datetime
+# Usage:
+# from helpers.dateutils import datetime_to_iso8061, iso8061_to_datetime, expiration_datetime
 #
-# *ISO8601
-# 2020-12-28T15:11:00.0000+09:00 (UTC以外)
-# 2020-12-28T15:11:00.0000Z (UTC)
+# Example:
+# current_datetime = datetime.now()
+# iso_date = datetime_to_iso8061(current_datetime)
+# converted_datetime = iso8061_to_datetime(iso_date)
+# expiration_date = expiration_datetime(72)
 #
-# *Basic rules
-# - The server will return the ISO 8061 format string in UTC time as
-#   "YYYY-MM-DDTHH:mm:ss.SSSS+timezone" in the device's time zone.
-# - The device will send the ISO 8061 format string in the device's time zone.
-# - The database will store all datetime in UTC no matter which area it is sent from.
-#
+
 import sys
-import os
 from flask import jsonify
 from pytz import timezone
 from datetime import datetime, timedelta
 sys.path.append('../')
 from libcommon.response.api_response import ErrorResponse, ErrorCode
 
-ISO8061_FORMAT = "%Y-%m-%d'T'%H:%M:%S.%f%z"
 
 class InvalidISOFormatError(Exception):
+    """Exception raised for invalid ISO 8061 formatted strings.
+
+    Attributes:
+        iso_formatted_string -- the invalid ISO 8061 string that caused the exception
+    """
+    
     def __init__(self, iso_formatted_string):
         self.iso_formatted_string = iso_formatted_string
 
     def __error_obj__(self):
-        error_response = ErrorResponse(
-            {
-                'code': ErrorCode.INVALID_PARAMETER.value,
-                'reason': ErrorCode.INVALID_PARAMETER.name,
-                'message': f'{self.iso_formatted_string} is invalid as iso formatted timestamp.'
-            }
-        )
+        """Constructs an error object for JSON response."""
+        error_response = ErrorResponse({
+            'code': ErrorCode.INVALID_PARAMETER.value,
+            'reason': ErrorCode.INVALID_PARAMETER.name,
+            'message': f'{self.iso_formatted_string} is invalid as iso formatted timestamp.'
+        })
         return jsonify({'saved_data': None, 'error': error_response.json()}), 400
 
     def __str__(self):
         return repr(f'{self.iso_formatted_string} is invalid as iso formatted timestamp.')
 
 
-def datetime_to_iso8061(date: datetime = None, tz = "Asia/Tokyo") -> str:
-    date_with_timezone = date.astimezone(timezone(tz))  # return the user's local time
-    iso_formatted = datetime.strftime(date_with_timezone, ISO8061_FORMAT)
-    return iso_formatted
+def datetime_to_iso8061(date: datetime = None, tz="Asia/Tokyo") -> str:
+    """Converts a datetime object to its ISO 8061 string representation.
+
+    Parameters:
+    - date: datetime object to be converted. Uses current date and time if not provided.
+    - tz: The timezone to convert to before formatting. Default is "Asia/Tokyo".
+
+    Returns: 
+    - ISO 8061 formatted string of the date.
+
+    Example:
+    >>> datetime_to_iso8061(datetime(2021, 1, 31, 16, 25, 8, 309648))
+    '2021-01-31T16:25:08.309648+09:00'
+    """
+    
+    if not date:
+        date = datetime.now()
+    date_with_timezone = date.astimezone(timezone(tz))
+    return date_with_timezone.isoformat()
 
 
-def iso8061_to_datetime(iso_formatted_string) -> datetime:
+def iso8061_to_datetime(iso_formatted_string: str) -> datetime:
+    """Converts an ISO 8061 string to its datetime object representation.
 
-    # temporary measures
-    # TODO: delete this code
-    if iso_formatted_string[10] == "T":
-        iso_formatted_string = iso_formatted_string.replace("T", "'T'")
-    iso_formatted_string = iso_formatted_string.replace(" 0900", "+0900")
+    Parameters:
+    - iso_formatted_string: The ISO 8061 string to be converted.
 
+    Returns: 
+    - datetime object representing the input string.
+
+    Raises:
+    - InvalidISOFormatError: If the input string is not a valid ISO 8061 format.
+
+    Example:
+    >>> iso8061_to_datetime('2021-01-31T16:25:08.309648+09:00')
+    datetime.datetime(2021, 1, 31, 16, 25, 8, 309648, tzinfo=<UTC>)
+    """
+    
     try:
-        date_with_timezone = datetime.strptime(
-            iso_formatted_string, ISO8061_FORMAT)
-        date_utc = date_with_timezone.astimezone(
-            timezone("UTC"))  # always save as UTC
+        date_with_timezone = datetime.fromisoformat(iso_formatted_string)
+        date_utc = date_with_timezone.astimezone(timezone("UTC"))
     except ValueError as e:
         raise InvalidISOFormatError(iso_formatted_string)
     else:
@@ -70,9 +92,24 @@ def iso8061_to_datetime(iso_formatted_string) -> datetime:
 
 
 def expiration_datetime(after_hours=48):
+    """Returns the datetime after a certain number of hours from now.
+
+    Parameters:
+    - after_hours: Number of hours from now for the expiration. Default is 48.
+
+    Returns: 
+    - datetime object of the expiration time.
+
+    Example:
+    >>> expiration_datetime(72)
+    datetime.datetime(2023, 9, 25, 7, 45, 12, 345678)  # Example time
+    """
+
     return datetime.now() + timedelta(hours=after_hours)
 
-if __name__ == "__main__": # Function Test
-    iso8061 = "2021-01-31'T'16:25:08.309648+0900"
-    print('datetime: {0}, convert to iso8061: {1}'.format(datetime_string, datetime_to_iso8061(date=datetime.now())))
-    print('iso8061: {0}, convert to datetime: {1}'.format(iso8061, iso8061_to_datetime(iso8061)))
+
+if __name__ == "__main__":
+    current_datetime = datetime.now()
+    iso8061 = datetime_to_iso8061(date=current_datetime)
+    print(f'datetime: {current_datetime}, convert to iso8061: {iso8061}')
+    print(f'iso8061: {iso8061}, convert to datetime: {iso8061_to_datetime(iso8061)}')
