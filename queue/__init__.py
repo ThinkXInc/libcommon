6fd8af34ab728b216da5ca9c346c00b919791220
kazukiotsuka <otsuka.kazuki@googlemail.com>
2023-12-28 15:22:56 +0900

    queue: remove queue (to llm)

--- queue/__init__.py
+++
@@ -1,4 +0,0 @@
-# TODO:
-#from libcommon.queue.queue_config import QueueConfig
-#from libcommon.queue.queue_squeue QueueServer, ReconnectingQueueServer
-#from libcommon.queue.queue_publisher import QueuePublisher, QueueConnectionManager
