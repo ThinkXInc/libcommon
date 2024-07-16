1567d19c20a40daf979451c6e2af4c9ad708d75d
kazukiotsuka <otsuka.kazuki@googlemail.com>
2024-07-16 17:01:48 +0900

    add get_or_create to mongomodel

--- mongomodel.py
+++ mongomodel.py
@@ -1,7 +1,13 @@
-from mongoengine import Document, StringField, DateTimeField, ObjectIdField
+from mongoengine import Document, StringField, DateTimeField, ObjectIdField, DoesNotExist
 from datetime import datetime
 import pytz
 from bson import ObjectId
+
+# Logger
+from libcommon.logger import Logger
+from libcommon.color import *
+logger = Logger()
+logger.setLevel(logger.INFO)
 
 class MongoModel(Document):
     # Generic Fields
@@ -30,3 +36,21 @@ class MongoModel(Document):
                 d[key] = str(value)
         return {x: d[x] for x in d if x not in excludes}
 
+    @classmethod
+    def get_or_create(cls, defaults=None, **kwargs):
+        defaults = defaults or {}
+        try:
+            obj = cls.objects(**kwargs).first()
+            if obj:
+                return obj, False
+            else:
+                obj = cls(**kwargs, **defaults)
+                obj.save()
+                return obj, True
+        except ValidationError as e:
+            logger.error(f'Validation error on creating {cls.__name__}: {e}')
+            raise
+
+    @classmethod
+    def count(cls):
+        return cls.objects.count()
